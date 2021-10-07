cf9076637af81f4ff96c4ae351d757355f5c9671
Vladislav Antonov <vladislav.antonov@gmail.com>
2021-10-07 09:42:01 +0300

    Add window example

---
+++ 1_Window/first_app.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "lve_window.hpp"
+
+namespace lve {
+
+class FirstApp {
+ public:
+  static constexpr int WIDTH = 800;
+  static constexpr int HEIGHT = 800;
+
+  void run();
+
+ private:
+  LveWindow lveWindow{WIDTH, HEIGHT, "Hello Vulkan!"};
+};
+
+}  // namespace lve
---
+++ 1_Window/lve_window.cpp
@@ -0,0 +1,26 @@
+#include "lve_window.hpp"
+
+namespace lve {
+
+LveWindow::LveWindow(int w, int h, const std::string& name)
+    : width{w}, height{h}, windowName{name} {
+  initWindow();
+}
+
+LveWindow::~LveWindow() {
+  glfwDestroyWindow(window);
+  glfwTerminate();
+}
+
+void LveWindow::initWindow() {
+  glfwInit();
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+  window =
+      glfwCreateWindow(width, height, windowName.c_str(), nullptr, nullptr);
+}
+
+bool LveWindow::shouldClose() { return glfwWindowShouldClose(window); }
+
+}  // namespace lve
