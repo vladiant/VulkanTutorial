585bb1375c14af504f46424cb821caa5b6614530
Vladislav Antonov <vladislav.antonov@gmail.com>
2021-10-26 09:14:42 +0300

    Add Swap chain overview sample

---
+++ 5a_SwapChainOverview/first_app.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+
+#include "lve_device.hpp"
+#include "lve_pipeline.hpp"
+#include "lve_swap_chain.hpp"
+#include "lve_window.hpp"
+
+namespace lve {
+
+class FirstApp {
+ public:
+  static constexpr int WIDTH = 800;
+  static constexpr int HEIGHT = 800;
+
+  FirstApp();
+  ~FirstApp();
+
+  FirstApp(const FirstApp&) = delete;
+  FirstApp& operator=(const FirstApp&) = delete;
+
+  void run();
+
+ private:
+  void createPipelineLayout();
+  void createPipeline();
+  void createCommandBuffers();
+  void drawFrame();
+
+  LveWindow lveWindow{WIDTH, HEIGHT, "Hello Vulkan!"};
+  LveDevice lveDevice{lveWindow};
+  LveSwapChain lveSwapChain{lveDevice, lveWindow.getExtent()};
+  std::unique_ptr<LvePipeline> lvePipeline;
+  VkPipelineLayout pipelineLayout;
+  std::vector<VkCommandBuffer> commandBuffers;
+};
+
+}  // namespace lve
