f31e98c324709e159da3bf6a98c7ff854e3675f9
Vladislav Antonov <vladislav.antonov@gmail.com>
2021-11-27 01:02:10 +0200

    Add @D Transformations sample

---
+++ 10_2Dtransformations/lve_model.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <vector>
+
+#include "lve_device.hpp"
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
+namespace lve {
+
+class LveDevice;
+
+class LveModel {
+ public:
+  struct Vertex {
+    glm::vec2 position;
+    glm::vec3 color;
+
+    static std::vector<VkVertexInputBindingDescription> getBindingDescription();
+    static std::vector<VkVertexInputAttributeDescription>
+    getAttributeDescription();
+  };
+
+  LveModel(LveDevice& lveDevice, const std::vector<Vertex>& vertices);
+  ~LveModel();
+
+  LveModel(const LveModel&) = delete;
+  LveModel& operator=(const LveModel&) = delete;
+
+  void bind(VkCommandBuffer commandBuffer);
+  void draw(VkCommandBuffer commandBuffer);
+
+ private:
+  void createVertexBuffers(const std::vector<Vertex>& vertices);
+
+  LveDevice& lveDevice;
+  VkBuffer vertexBuffer;
+  VkDeviceMemory vertexBufferMemory;
+  uint32_t vertexCount;
+};
+
+}  // namespace lve
