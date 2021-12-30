8e6c2c2394596110420b7110d4a7b4addafbb6cf
Vladislav Antonov <vladislav.antonov@gmail.com>
2021-12-30 23:50:48 +0200

    Add Game Loops User Input sample

---
+++ 15_GameLoopsUserInput/first_app.cpp
@@ -0,0 +1,134 @@
+#include "first_app.hpp"
+
+#include <array>
+#include <chrono>
+#include <iostream>
+#include <stdexcept>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
+
+#include "keyboard_movement_controller.hpp"
+#include "lve_camera.hpp"
+#include "simple_render_system.hpp"
+
+namespace lve {
+
+FirstApp::FirstApp() { loadGameObjects(); }
+
+FirstApp::~FirstApp() {}
+
+// temporary helper function, creates a 1x1x1 cube centered at offset
+std::unique_ptr<LveModel> createCubeModel(LveDevice& device, glm::vec3 offset) {
+  std::vector<LveModel::Vertex> vertices{
+
+      // left face (white)
+      {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+      {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+      {{-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
+      {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+      {{-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
+      {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+
+      // right face (yellow)
+      {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+      {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+      {{.5f, -.5f, .5f}, {.8f, .8f, .1f}},
+      {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+      {{.5f, .5f, -.5f}, {.8f, .8f, .1f}},
+      {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+
+      // top face (orange, remember y axis points down)
+      {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+      {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+      {{-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+      {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+      {{.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+      {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+
+      // bottom face (red)
+      {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+      {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+      {{-.5f, .5f, .5f}, {.8f, .1f, .1f}},
+      {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+      {{.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+      {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+
+      // nose face (blue)
+      {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+      {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+      {{-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+      {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+      {{.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+      {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+
+      // tail face (green)
+      {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+      {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+      {{-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+      {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+      {{.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+      {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+
+  };
+  for (auto& v : vertices) {
+    v.position += offset;
+  }
+  return std::make_unique<LveModel>(device, vertices);
+}
+
+void FirstApp::loadGameObjects() {
+  std::shared_ptr<LveModel> LveModel =
+      createCubeModel(lveDevice, {0.0f, 0.0f, 0.0f});
+  auto cube = LveGameObject::createGameObject();
+  cube.model = LveModel;
+  cube.transform.translation = {0.0f, 0.0f, 2.5f};
+  cube.transform.scale = {0.5f, 0.5f, 0.5f};
+  gameObjects.push_back(std::move(cube));
+}
+
+void FirstApp::run() {
+  SimpleRenderSystem simpleRenderSystem{lveDevice,
+                                        lveRenderer.getSwapChainRenderPass()};
+  LveCamera camera{};
+
+  auto viewerObject = LveGameObject::createGameObject();
+  KeyboardMovementController cameraController{};
+
+  auto currentTime = std::chrono::high_resolution_clock::now();
+
+  while (!lveWindow.shouldClose()) {
+    glfwPollEvents();
+
+    auto newTime = std::chrono::high_resolution_clock::now();
+    float frameTime =
+        std::chrono::duration<float, std::chrono::seconds::period>(newTime -
+                                                                   currentTime)
+            .count();
+    currentTime = newTime;
+
+    // To limit the frame time
+    // frameTime = std::min(frameTime, MAX_FRAME_TIME);
+
+    cameraController.moveInPlaneXZ(lveWindow.getGLFWwindow(), frameTime,
+                                   viewerObject);
+    camera.setViewYXZ(viewerObject.transform.translation,
+                      viewerObject.transform.rotation);
+
+    float aspect = lveRenderer.getAspectRatio();
+    camera.setPerspectiveProjection(glm::radians(50.0f), aspect, 0.1f, 10.0f);
+
+    if (auto commandBuffer = lveRenderer.beginFrame()) {
+      lveRenderer.beginSwapChainRenderPass(commandBuffer);
+      simpleRenderSystem.renderGameObjects(commandBuffer, gameObjects, camera);
+      lveRenderer.endSwapChainRenderPass(commandBuffer);
+      lveRenderer.endFrame();
+    }
+  }
+
+  vkDeviceWaitIdle(lveDevice.device());
+}
+
+}  // namespace lve
