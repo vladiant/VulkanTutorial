bfcb0ca91d4ce7bb5d637dcae690c2383916ba5b
Vladislav Antonov <vladislav.antonov@gmail.com>
2022-01-27 08:54:34 +0200

    Add Diffuse Shading sample

---
+++ 18_DiffuseShading/first_app.cpp
@@ -0,0 +1,82 @@
+#include "first_app.hpp"
+
+#include <array>
+#include <chrono>
+#include <iostream>
+#include <stdexcept>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
+
+#include "keyboard_movement_controller.hpp"
+#include "lve_camera.hpp"
+#include "simple_render_system.hpp"
+
+namespace lve {
+
+FirstApp::FirstApp() { loadGameObjects(); }
+
+FirstApp::~FirstApp() {}
+
+void FirstApp::loadGameObjects() {
+  std::shared_ptr<LveModel> lveModel =
+      LveModel::createmodelFromFile(lveDevice, "models/flat_vase.obj");
+  auto flatVase = LveGameObject::createGameObject();
+  flatVase.model = lveModel;
+  flatVase.transform.translation = {-0.5f, 0.5f, 2.5f};
+  flatVase.transform.scale = {3.0f, 1.5f, 3.0f};  // glm::vec3(3.0f);
+  gameObjects.push_back(std::move(flatVase));
+
+  lveModel = LveModel::createmodelFromFile(lveDevice, "models/smooth_vase.obj");
+  auto smoothVase = LveGameObject::createGameObject();
+  smoothVase.model = lveModel;
+  smoothVase.transform.translation = {0.5f, 0.5f, 2.5f};
+  smoothVase.transform.scale = {3.0f, 1.5f, 3.0f};  // glm::vec3(3.0f);
+  gameObjects.push_back(std::move(smoothVase));
+}
+
+void FirstApp::run() {
+  SimpleRenderSystem simpleRenderSystem{lveDevice,
+                                        lveRenderer.getSwapChainRenderPass()};
+  LveCamera camera{};
+
+  auto viewerObject = LveGameObject::createGameObject();
+  KeyboardMovementController cameraController{};
+
+  auto currentTime = std::chrono::high_resolution_clock::now();
+
+  while (!lveWindow.shouldClose()) {
+    glfwPollEvents();
+
+    auto newTime = std::chrono::high_resolution_clock::now();
+    float frameTime =
+        std::chrono::duration<float, std::chrono::seconds::period>(newTime -
+                                                                   currentTime)
+            .count();
+    currentTime = newTime;
+
+    // To limit the frame time
+    // frameTime = std::min(frameTime, MAX_FRAME_TIME);
+
+    cameraController.moveInPlaneXZ(lveWindow.getGLFWwindow(), frameTime,
+                                   viewerObject);
+    camera.setViewYXZ(viewerObject.transform.translation,
+                      viewerObject.transform.rotation);
+
+    float aspect = lveRenderer.getAspectRatio();
+    camera.setPerspectiveProjection(glm::radians(50.0f), aspect, 0.1f, 10.0f);
+
+    if (auto commandBuffer = lveRenderer.beginFrame()) {
+      lveRenderer.beginSwapChainRenderPass(commandBuffer);
+      simpleRenderSystem.renderGameObjects(commandBuffer, gameObjects, camera);
+      lveRenderer.endSwapChainRenderPass(commandBuffer);
+      lveRenderer.endFrame();
+    }
+  }
+
+  vkDeviceWaitIdle(lveDevice.device());
+}
+
+}  // namespace lve
