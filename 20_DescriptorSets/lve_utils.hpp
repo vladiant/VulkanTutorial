75336566643372a8fd6cc1fe65c4d7508629f13b
Vladislav Antonov <vladislav.antonov@gmail.com>
2022-10-13 08:24:29 +0300

    Remove extra symbol

--- 20_DescriptorSets/lve_utils.hpp
+++ 20_DescriptorSets/lve_utils.hpp
@@ -7,6 +7,6 @@ template <typename T, typename... Rest>
 void hashCombine(std::size_t& seed, const T& v, const Rest&... rest) {
   seed ^= std::hash<T>{}(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
   (hashCombine(seed, rest), ...);
-};
+}
 
 }  // namespace lve
